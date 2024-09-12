a15de8514e02a0442bd9e18264898a0d3178491c
Mitesh13499 <mitesh@mobiux.in>
2024-09-12 11:31:51 +0530

    chore: glide & sass installed, feat: basic carousel

---
+++ public/sampleResponse.js
@@ -0,0 +1,263 @@
+const sampleResponse = [
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+  {
+    uuid: '06660518-97a8-729e-8000-9f068ea046af',
+    title: 'Mixkit-Mother-With',
+    description: '',
+    duration: '00:00:10',
+    thumbnail:
+      'https://thumbnail.dietpixels.net/media/06660518-97a8-729e-8000-9f068ea046af/thumbnail.png?w=200&format=webp&quality=70',
+    products: [
+      {
+        uuid: '066d95ae-920c-7cfb-8000-1d2bc0c3056d',
+        link: 'https://printo.in/categories/business-cards/customizable-products/standard-material-cards',
+        display_title: 'Standard Business Card',
+        description:
+          'Make a lasting impression with our Standard Business Cards, designed to communicate your professional identity effectively.',
+        thumbnail:
+          'https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-06-pbls2m0-min.jpg',
+        display_price: '0.00',
+        compare_price: '0.00',
+        is_out_of_stock: false,
+      },
+      {
+        uuid: '066d95ae-209f-72c9-8000-e3d6caeba9bd',
+        link: 'https://printo.in/categories/business-cards/customizable-products/non-tearable-business-card',
+        display_title: 'Non Tearable Business Card',
+        description:
+          'Choose Non-Tearable Business Cards for ultimate durability and a lasting impression.',
+        thumbnail:
+          'https://printo-s3.dietpixels.net/cloudinary/res/dxivtqnri/image/upload/2023/Businesscard/Product-page/Non-Terabble-Card/1685032432.jpg?quality=70&format=webp&w=640',
+        display_price: '500.00',
+        compare_price: '500.00',
+        is_out_of_stock: false,
+      },
+    ],
+    height: 1920,
+    width: 1080,
+    aspect_ratio: '1080:1920',
+  },
+];
+
+export default sampleResponse;
--- src/my-element.ts
+++ src/my-element.ts
@@ -1,6 +1,30 @@
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {LitElement, html, css, PropertyValues} from 'lit';
+import {customElement, property, state} from 'lit/decorators.js';
+import Glide from '@glidejs/glide';
 
+import sampleResponse from '../public/sampleResponse.js';
+interface Product {
+  uuid: string;
+  link: string;
+  display_title: string;
+  description: string;
+  thumbnail: string;
+  display_price: string;
+  compare_price: string;
+  is_out_of_stock: boolean;
+}
+interface Video {
+  uuid: string;
+  title: string;
+  description: string;
+  duration: string;
+  thumbnail: string;
+
+  products: Product[];
+  height: number;
+  width: number;
+  aspect_ratio: string;
+}
 /**
  * An example element.
  *
@@ -10,14 +34,69 @@ import {customElement, property} from 'lit/decorators.js';
  */
 @customElement('my-element')
 export class MyElement extends LitElement {
-  static override styles = css`
-    :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
-    }
-  `;
+  static override styles = [
+    css`
+      :host {
+      }
+      .glide {
+        position: relative;
+        width: 100%;
+        box-sizing: border-box;
+      }
+      .glide * {
+        box-sizing: inherit;
+      }
+      .glide__track {
+        overflow: hidden;
+      }
+      .glide__slides {
+        position: relative;
+        width: 100%;
+        list-style: none;
+        backface-visibility: hidden;
+        transform-style: preserve-3d;
+        touch-action: pan-Y;
+        overflow: hidden;
+        margin: 0;
+        padding: 0;
+        white-space: nowrap;
+        display: flex;
+        flex-wrap: nowrap;
+        will-change: transform;
+      }
+      .glide__slides--dragging {
+        user-select: none;
+      }
+      .glide__slide {
+        width: 100%;
+        height: 100%;
+        flex-shrink: 0;
+        white-space: normal;
+        user-select: none;
+        -webkit-touch-callout: none;
+        -webkit-tap-highlight-color: transparent;
+      }
+      .glide__slide a {
+        user-select: none;
+        -webkit-user-drag: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+      }
+      .glide__arrows {
+        -webkit-touch-callout: none;
+        user-select: none;
+      }
+      .glide__bullets {
+        -webkit-touch-callout: none;
+        user-select: none;
+      }
+      .glide--rtl {
+        direction: rtl;
+      }
+
+      /*# sourceMappingURL=output.css.map */
+    `,
+  ];
 
   /**
    * The name to say "Hello" to.
@@ -31,28 +110,78 @@ export class MyElement extends LitElement {
   @property({type: Number})
   count = 0;
 
-  override render() {
-    return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `;
+  @property()
+  playlist = '';
+
+  @state()
+  videos: Video[] = [];
+
+  protected override updated(_changedProperties: PropertyValues): void {
+    console.log('_changedProperties', _changedProperties);
+    if (_changedProperties.has('videos') && this.videos.length > 0) {
+      const root = this.shadowRoot?.querySelector('.glide');
+      new Glide(root, {
+        type: 'carousel',
+        perView: 3,
+      }).mount();
+    }
   }
 
-  private _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
+  private async getData() {
+    const response = await fetch(
+      `https://app.dietpixels.com/api/v1/public/playlists/${this.playlist}`
+    );
+    const res = await response.json();
+    console.log('res', res);
+    this.videos = sampleResponse;
+  }
+  private getVideoElement(video) {
+    const slider = this.shadowRoot?.querySelector('.main-slider');
+    const li = document.createElement('li');
+    li.classList.add('glide__slide');
+    li.textContent = video.title;
+    slider?.appendChild(li);
+    // <li class="glide__slide">${video.title}</li>
+  }
+  override connectedCallback(): void {
+    super.connectedCallback();
+    this.getData();
+  }
+  override render() {
+    return html`
+      <div class="glide">
+        <div class="glide__track" data-glide-el="track">
+          <ul class="glide__slides main-slider">
+            ${this.videos.map(
+              (video) => html`<li class="glide__slide">
+                <!-- ${video.title} -->
+                <img
+                  src="https://cdn.printshoppy.com/image/cache/catalog/product-image/mobile-cases/google-pixel-xl/google-pixel-xl-304-600x800.jpg"
+                />
+              </li>`
+            )}
+            <!-- <li class="glide__slide">0</li>
+            <li class="glide__slide">1</li>
+            <li class="glide__slide">2</li> -->
+          </ul>
+        </div>
+        <div class="glide__arrows" data-glide-el="controls">
+          <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+            prev
+          </button>
+          <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+            next
+          </button>
+        </div>
+      </div>
+      <slot></slot>
+    `;
   }
 
   /**
    * Formats a greeting
    * @param name The name to say "Hello" to
    */
-  sayHello(name: string): string {
-    return `Hello, ${name}`;
-  }
 }
 
 declare global {
