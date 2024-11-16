9295c320ebc1e1de5d774693cd8fb0fb9a3ed1a6
Amal Salvin Joseph <amal@mobiux.in>
2024-11-16 14:10:40 +0530

    feat: add padding

--- src/mobi-carousel.ts
+++ src/mobi-carousel.ts
@@ -2,18 +2,12 @@ import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import './components/carousel/carousel';
 
-/**
- * An example element.
- *
- * @fires count-changed - Indicates when the count changes
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement('mobi-carousel')
 export class MobiCarousel extends LitElement {
   static override styles = css`
     :host {
       box-sizing: border-box;
+      padding: 1rem 0;
     }
   `;
 
