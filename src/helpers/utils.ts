88aed8c0d7b9c98e3e182306f8cced80f1e04a54
vyshakha@mobiux.in <vyshakhams>
2024-12-05 21:57:50 +0530

    feat(card): added product cards with styles

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -27,7 +27,7 @@ export function changeActiveReelParam(param: string, value: string) {
   history.replaceState(null, '', newUrl);
 }
 
-export function formatPrice(price: Number) {
+export function formatPrice(price: number) {
   const formatter = new Intl.NumberFormat('en-IN', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
